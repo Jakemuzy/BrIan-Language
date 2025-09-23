7eec4a63928c05acde7f72b55acaff4aa5dff96e
Jake <jmuzyka4@gmail.com>
2025-09-23 12:11:29 -0400

    Finalied compiler script

--- compile.sh
+++ compile.sh
@@ -7,24 +7,18 @@ INCLUDE_PATH="includes"
 INCLUDES=$(echo "$INCLUDE_PATH/"*.h)
 SRC_PATH="src"
 SOURCES=$(echo "$SRC_PATH/"*.c)
-
 BUILD_PATH="builds"
-PROG_NAME=$1
-if [ $# -lt 2 ]
-then
-	PROG_NAME="Tokenizer"
-fi
 
 GCC_FLAGS="$GCC_FLAGS -I$INCLUDE_PATH"
-GCC_FLAGS="$GCC_FLAGS $PROG_NAME.c -o $BUILD_PATH/$PROG_NAME"
 
-OLD_IFS=$IFS
+# Compile each SRC file separately
 IFS=' '
-
-for header in $INCLUDES
+for SRC in $SOURCES
 do
-	echo $header
-done
+	EXEC="${SRC//$SRC_PATH/}"
+	EXEC="${EXEC//.c/}"	
+	CUR_EXEC="$GCC_FLAGS $SRC -o $BUILD_PATH$EXEC"
 
-echo $GCC_FLAGS
-#gcc $GCC_FLAGS 
+	printf "COMPILING WITH FLAGS:\n\t$CUR_EXEC\n" 
+	gcc "$CUR_EXEC"
+done
