76f0e03a8696282c8231ccd2c09b9796485fba86
Jake <jmuzyka4@gmail.com>
2025-11-13 14:45:16 -0500

    Parser Beginnings

---
+++ includes/Parser.h
@@ -0,0 +1,16 @@
+#include "Tokenizer.h"
+
+typedef struct ASTNode
+{
+    struct ASTNode* children; 
+    int count;
+    Token t;
+} ASTNode;
+
+typedef struct AST
+{
+    ASTNode* root;
+} AST;
+
+AST* ConstructAST();
+
---
+++ src/Parser.c
@@ -0,0 +1,10 @@
+#include "Parser.h"
+
+AST* ConstructAST()
+{
+    AST tree;
+    tree.root = malloc(sizeof(ASTNode));
+
+
+    return;
+}
